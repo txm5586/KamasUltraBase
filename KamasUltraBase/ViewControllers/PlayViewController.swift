30463361f81e058084c2696a83747745d2d37d93
Tassio Marques <tassiomm@gmail.com>
2017-12-17 14:08:26 +0100

    functiong create data

--- KamasUltraBase/ViewControllers/PlayViewController.swift
+++ KamasUltraBase/ViewControllers/PlayViewController.swift
@@ -17,17 +17,45 @@ class PlayViewController: UIViewController {
         appDelegate = UIApplication.shared.delegate as! AppDelegate
         appDelegate.ppService.delegate = self
         // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(PlayViewController.didReceiveData(notification:)), name:Notifications.MPCDidReceiveData, object: nil);
     }
     
+    // MARK: - Actions
     @IBAction func tappedChangeBack(_ sender: Any) {
-        self.appDelegate.ppService.send(dataInfo: "Any")
+        appDelegate.ppService.send(dataInfo: "color")
     }
-
+    
+    // MARK: - Notifications objc funcs
+    @objc func didReceiveData(notification: NSNotification) {
+        let userInfo = NSDictionary(dictionary: notification.userInfo!)
+        
+        let peerID = userInfo.object(forKey: Notifications.keyPeerID) as! MCPeerID
+        let data = userInfo.object(forKey: Notifications.keyData) as! String
+        
+        if let peer = Global.shared.connectedPeer, peer == peerID {
+            print("Changed by \(peer.displayName) with data \(data)")
+            
+            let rd = arc4random() % 5
+            switch rd {
+            case 0:
+                self.view.backgroundColor = UIColor.cyan
+            case 1:
+                self.view.backgroundColor = UIColor.green
+            case 2:
+                self.view.backgroundColor = UIColor.blue
+            case 3:
+                self.view.backgroundColor = UIColor.brown
+            case 4:
+                self.view.backgroundColor = UIColor.purple
+            case 5:
+                self.view.backgroundColor = UIColor.gray
+            default:
+                self.view.backgroundColor = UIColor.black
+            }
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
