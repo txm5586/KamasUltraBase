e0c7fd2cd031999a8482057422f84caa4e9259b1
Cristiano Maria Coppotelli <kikko951@gmail.com>
2017-12-21 12:06:53 +0100

    Integrated view's background gradients animation into ActionViewController.

--- KamasUltraBase/ActionViewController.swift
+++ KamasUltraBase/ActionViewController.swift
@@ -8,14 +8,27 @@
 
 import UIKit
 
-class ActionViewController: UIViewController {
-
+class ActionViewController: UIViewController, CAAnimationDelegate {
+    
+    var gradientLayer: CAGradientLayer!
+    var fromValue = [MoodConfig.gradientColor1,
+                     MoodConfig.gradientColor2,
+                     MoodConfig.gradientColor3,
+                     MoodConfig.gradientColor4]
+    var toValue = [MoodConfig.gradientColor3,
+                   MoodConfig.gradientColor4,
+                   MoodConfig.gradientColor1,
+                   MoodConfig.gradientColor2]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         print("whats happenings?")
         // Do any additional setup after loading the view.
         NotificationCenter.default.addObserver(self, selector: #selector(ActionViewController.lostConnectionWithPeer(notification:)), name:Notifications.DidLostConnectionWithPeer, object: nil);
+        
+        changeBackground()
+        transitionGradients()
     }
     
     @objc func lostConnectionWithPeer(notification: NSNotification) {
@@ -54,6 +67,37 @@ class ActionViewController: UIViewController {
                 performSegue(withIdentifier: "unwindAsGuestSegue", sender: self)
             }
         }
+    }
+    
+    func changeBackground () {
+        gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [MoodConfig.gradientColor1,
+                                MoodConfig.gradientColor2,
+                                MoodConfig.gradientColor3,
+                                MoodConfig.gradientColor4]
+        gradientLayer.locations = [0,0.45,0.55,1]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        view.layer.addSublayer(gradientLayer)
+    }
+    
+    func transitionGradients () {
+        let gradientChangeAnimation = CABasicAnimation(keyPath: "colors")
+        gradientChangeAnimation.delegate = self
+        gradientChangeAnimation.duration = 5.0
+        gradientChangeAnimation.fromValue = fromValue
+        gradientChangeAnimation.toValue = toValue
+        gradientChangeAnimation.fillMode = kCAFillModeForwards
+        gradientChangeAnimation.isRemovedOnCompletion = false
+        gradientLayer.add(gradientChangeAnimation, forKey: "colorChange")
+    }
+    
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        let swapFromValue = fromValue
+        fromValue = toValue
+        toValue = swapFromValue
+        transitionGradients()
     }
     
     @IBAction func unwindToActionFlow(segue:UIStoryboardSegue) {
