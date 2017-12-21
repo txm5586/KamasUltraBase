cf5ea9195b5529dac777b49a85933d7894c49acd
Tassio Marques <tassiomm@gmail.com>
2017-12-21 14:15:10 +0100

    Update adjusts

--- KamasUltraBase/ActionViewController.swift
+++ KamasUltraBase/ActionViewController.swift
@@ -16,6 +16,37 @@ class ActionViewController: UIViewController {
         print("whats happenings?")
         // Do any additional setup after loading the view.
         NotificationCenter.default.addObserver(self, selector: #selector(ActionViewController.lostConnectionWithPeer(notification:)), name:Notifications.DidLostConnectionWithPeer, object: nil);
+        
+        skipActionScreen()
+    }
+    
+    func skipActionScreen() {
+        Global.shared.isMasterTurn = !Global.shared.isMasterTurn
+        
+        if Global.shared.isMaster {
+            // Check Turn
+            // IF TURN
+            // UNWIND AS HOST
+            if Global.shared.isMasterTurn {
+                print("----- Is going unwind as Host ------")
+                performSegue(withIdentifier: "unwindAsHostSegue", sender: self)
+            } else {
+                print("----- Is going to restard as guest ------")
+                performSegue(withIdentifier: "restartAsGuestSegue", sender: self)
+            }
+            // IF NOT TURN
+            
+        } else {
+            // Check Turn
+            // IF TURN
+            if !Global.shared.isMasterTurn {
+                performSegue(withIdentifier: "restartAsHostSegue", sender: self)
+                // IF NOT TURN
+                // UNWIND AS GUEST
+            } else {
+                performSegue(withIdentifier: "unwindAsGuestSegue", sender: self)
+            }
+        }
     }
     
     @objc func lostConnectionWithPeer(notification: NSNotification) {
